a2a9be64c36bb639d081ae9ae721912b7c2b90f4
Jhair01 <99994963+Jhair01@users.noreply.github.com>
2022-09-26 14:52:54 -0500

    Update index.php
    Librería Materia

--- views/materia/index.php
+++ views/materia/index.php
@@ -5,6 +5,7 @@ use yii\helpers\Url;
 use yii\grid\ActionColumn;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+use app\models\Materia;
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\MateriaSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
