6d87d497d9cf8be67e86701fedaba0ca3986697c
Jhair01 <99994963+Jhair01@users.noreply.github.com>
2022-09-27 14:23:16 -0500

    Update index.php
    Hola

--- views/site/index.php
+++ views/site/index.php
@@ -14,21 +14,21 @@ $this->title = 'Escuela';
     <div class="row">
         <div class="col-lg-4">
             <h2>Alumnos</h2>
-            <p>Accederl al menú de los alumnos.</p>
+            <p>Acceder al menú de los alumnos.</p>
             <?= Html::a(Yii::t('app', 'Alumno'), ['/alumno/index'], ['class' => 'btn btn-primary']) ?>
         </div>
 
         <!--cuadricula de maestros-->
         <div class="col-lg-4">
             <h2>Maestros</h2>
-            <p>Accederl al menú de los maestros.</p>
+            <p>Acceder al menú de los maestros.</p>
             <?= Html::a(Yii::t('app', 'Maestro'), ['/maestro/index'], ['class' => 'btn btn-primary']) ?>
         </div>
 
         <!--cuadricula de administrativo-->
         <div class="col-lg-4">
             <h2>Administrativos</h2>
-            <p>Accederl al menú del personal Administrativo.</p>
+            <p>Acceder al menú del personal Administrativo.</p>
             <?= Html::a(Yii::t('app', 'Administrativo'), ['/administrativo/index'], ['class' => 'btn btn-primary']) ?>
         </div>
         
