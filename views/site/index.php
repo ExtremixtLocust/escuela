712d09d90fc94394e9ed613e792d15a0207f8c39
Bryan Alejandro Zavala García <alex.zavala.b@gmail.com>
2022-09-28 03:20:22 -0500

    MenuCRUDS v1
    Se aumento la altura de las filas y se añadieron imagenes.

--- views/site/index.php
+++ views/site/index.php
@@ -11,11 +11,12 @@ $this->title = 'Escuela';
 
     <div class="body-content">
 
-    <div class="row">
+    <div class="row" style="height: 18rem;">
         <div class="col-lg-4">
             <center> <!--Centra los elementos--> 
             <h2>Alumnos</h2>
-            <p>Acceder al menú de los alumnos.</p>
+            <?php echo Html::img('@web/img/alumnos.png', ['style' => 'height: 10rem;']) ?>
+            <p class="pt-3">Acceder al menú de los alumnos.</p>
             <?= Html::a(Yii::t('app', 'Ver'), ['/alumno/index'], ['class' => 'btn btn-primary']) ?></center>
         </div>
 
@@ -23,7 +24,8 @@ $this->title = 'Escuela';
         <div class="col-lg-4">
             <center> <!--Centra los elementos-->
             <h2>Maestros</h2>
-            <p>Acceder al menú de los maestros.</p>
+            <?php echo Html::img('@web/img/maestros.png', ['style' => 'height: 10rem;']) ?>
+            <p class="pt-3">Acceder al menú de los maestros.</p>
             <?= Html::a(Yii::t('app', 'Ver'), ['/maestro/index'], ['class' => 'btn btn-primary']) ?></center>
         </div>
 
@@ -32,68 +34,77 @@ $this->title = 'Escuela';
         <div class="col-lg-4">
             <center> <!--Centra los elementos--> 
             <h2>Administrativos</h2>
-            <p>Acceder al menú del personal Administrativo.</p>
+            <?php echo Html::img('@web/img/administrativos.png', ['style' => 'height: 10rem;']) ?>
+            <p class="pt-3">Acceder al menú del personal Administrativo.</p>
             <?= Html::a(Yii::t('app', 'Ver'), ['/administrativo/index'], ['class' => 'btn btn-primary']) ?></center>
         </div>
         
     </div>
     <br>
     <!--segunda fila de elementos-->
-    <div class="row">
+    <div class="row" style="height: 18rem;">
         <div class="col-lg-4">
         <center> <!--Centra los elementos--> 
             <h2>Retículas</h2>
-            <p>Acceder al menú de las retículas.</p>
+            <?php echo Html::img('@web/img/reticulas.png', ['style' => 'height: 10rem;']) ?>
+            <p class="pt-3">Acceder al menú de las retículas.</p>
             <?= Html::a(Yii::t('app', 'Ver'), ['/reticula/index'], ['class' => 'btn btn-primary']) ?></center>
         </div>
         <div class="col-lg-4">
         <center> <!--Centra los elementos--> 
             <h2>Departamentos</h2>
-            <p>Acceder al menú de los departamentos.</p>
+            <?php echo Html::img('@web/img/departamentos.png', ['style' => 'height: 10rem;']) ?>
+            <p class="pt-3">Acceder al menú de los departamentos.</p>
             <?= Html::a(Yii::t('app', 'Ver'), ['/departamento/index'], ['class' => 'btn btn-primary']) ?></center>
         </div>
         <div class="col-lg-4">
         <center> <!--Centra los elementos--> 
             <h2>Trabajador</h2>
-            <p>Acceder al menú de los trabajadores.</p>
+            <?php echo Html::img('@web/img/trabajadores.png', ['style' => 'height: 10rem;']) ?>
+            <p class="pt-3">Acceder al menú de los trabajadores.</p>
             <?= Html::a(Yii::t('app', 'Ver'), ['/trabajador/index'], ['class' => 'btn btn-primary']) ?></center>
         </div>
     </div>
     <br>
     <!--tercera fila de elementos-->
-    <div class="row">
+    <div class="row" style="height: 18rem;">
         <div class="col-lg-4">
         <center> <!--Centra los elementos--> 
             <h2>Inventario</h2>
-            <p>Acceder al menú del inventario.</p>
+            <?php echo Html::img('@web/img/inventario.png', ['style' => 'height: 10rem;']) ?>
+            <p class="pt-3">Acceder al menú del inventario.</p>
             <?= Html::a(Yii::t('app', 'Ver'), ['/inventario/index'], ['class' => 'btn btn-primary']) ?></center>
         </div>
         <div class="col-lg-4">
         <center> <!--Centra los elementos-->
             <h2>Proveedores</h2>
-            <p>Acceder al menú de los proveedores.</p>
+            <?php echo Html::img('@web/img/proveedores.png', ['style' => 'height: 10rem;']) ?>
+            <p class="pt-3">Acceder al menú de los proveedores.</p>
             <?= Html::a(Yii::t('app', 'Ver'), ['/proveedor/index'], ['class' => 'btn btn-primary']) ?> </center>
         </div>
         <div class="col-lg-4">
         <center> <!--Centra los elementos--> 
             <h2>Materia</h2>
-            <p>Acceder al menú de las materias.</p>
+            <?php echo Html::img('@web/img/materias.png', ['style' => 'height: 10rem;']) ?>
+            <p class="pt-3">Acceder al menú de las materias.</p>
             <?= Html::a(Yii::t('app', 'Ver'), ['/materia/index'], ['class' => 'btn btn-primary']) ?></center>
         </div>
     </div>
     <br>
     <!--cuarta fila de elementos-->
-    <div class="row">
+    <div class="row" style="height: 18rem;">
         <div class="col-lg-4">
         <center> <!--Centra los elementos--> 
             <h2>Grupos</h2>
-            <p>Acceder al menú de grupos.</p>
+            <?php echo Html::img('@web/img/grupos.png', ['style' => 'height: 10rem;']) ?>
+            <p class="pt-3">Acceder al menú de grupos.</p>
             <?= Html::a(Yii::t('app', 'Ver'), ['/grupo/index'], ['class' => 'btn btn-primary']) ?></center>
         </div>
         <div class="col-lg-4">
         <center> <!--Centra los elementos-->
             <h2>Aulas</h2>
-            <p>Acceder al menú de las aulas.</p>
+            <?php echo Html::img('@web/img/aulas.png', ['style' => 'height: 10rem;']) ?>
+            <p class="pt-3">Acceder al menú de las aulas.</p>
             <?= Html::a(Yii::t('app', 'Ver'), ['/aula/index'], ['class' => 'btn btn-primary']) ?> </center>
         </div>
     </div>
